f04cfbc0b5ca67a3c1135b026de9f13481b0bdab
definitepotato <definitepotato@gmail.com>
2024-09-08 18:51:02 -0400

    task() zig 2015 day 5 part 2.

--- zig/2015/day5/main.zig
+++ zig/2015/day5/main.zig
@@ -67,6 +67,63 @@ fn has_naughty_string(s: []const u8) bool {
     return false;
 }
 
+fn has_two_nice_letters(s: []const u8) bool {
+    for (0..s.len) |idx| {
+        if (idx + 2 > s.len - 1) {
+            break;
+        }
+
+        if (s[idx] == s[idx + 2]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn has_pair_from(s: []const u8, start: usize, target: [2]u8) bool {
+    var window_left: usize = start;
+    var window_right: usize = window_left + 1;
+
+    for (s.len) |_| {
+        if (window_right > s.len - 1) {
+            break;
+        }
+
+        if (s[window_left] == target[0]) {
+            if (s[window_right] == target[1]) {
+                return true;
+            }
+        }
+
+        window_left += 1;
+        window_right += 1;
+    }
+
+    return false;
+}
+
+fn has_nice_pair(s: []const u8) bool {
+    var window_left: usize = 0;
+    var window_right: usize = 1;
+
+    for (s.len) |_| {
+        if (window_right > s.len - 1) {
+            break;
+        }
+
+        const result = has_pair_from(s, window_right + 1, [2]u8{ s[window_left], s[window_right] });
+        if (result == true) {
+            return true;
+        }
+
+        window_left += 1;
+        window_right += 1;
+    }
+
+    return false;
+}
+
 fn is_nice(s: []const u8) bool {
     // Must contain at least 3 vowels.
     if (num_vowels(s) < 3) {
@@ -86,15 +143,33 @@ fn is_nice(s: []const u8) bool {
     return true;
 }
 
+fn is_actually_nice(s: []const u8) bool {
+    // Must contain two letters that appear twice without overlapping.
+    if (has_nice_pair(s)) {
+        // Must contain at least one letter which repeats with exactly one letter between them.
+        if (has_two_nice_letters(s)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 pub fn main() !void {
-    var nice_lines: i32 = 0;
+    var nice_lines: usize = 0;
+    var actually_nice_lines: usize = 0;
 
     var lines = std.mem.tokenize(u8, input, "\n");
     while (lines.next()) |line| {
         if (is_nice(line)) {
             nice_lines += 1;
         }
+
+        if (is_actually_nice(line)) {
+            actually_nice_lines += 1;
+        }
     }
 
     print("Part 1: {d}\n", .{nice_lines});
+    print("Part 2: {d}\n", .{actually_nice_lines});
 }
