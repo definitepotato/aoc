3c8bf231e2484acd9de6ac338d42b54bb06db9ca
definitepotato <definitepotato@gmail.com>
2024-09-09 17:00:33 -0400

    task() zig 2015 day 4 part 1 and part 2.

--- zig/2015/day4/main.zig
+++ zig/2015/day4/main.zig
@@ -1,4 +1,38 @@
 const std = @import("std");
 const print = std.debug.print;
+const crypto = std.crypto;
+const expect = std.testing.expect;
 
-pub fn main() !void {}
+fn mine_string(input: []const u8, target: []const u8) !usize {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const md5 = crypto.hash.Md5;
+
+    var dec: usize = 1;
+    while (true) : (dec += 1) {
+        const b = try std.fmt.allocPrint(allocator, "{s}{d}", .{ input, dec });
+        defer allocator.free(b);
+
+        var buf: [md5.digest_length]u8 = undefined;
+        md5.hash(b, &buf, .{});
+
+        const hex = try std.fmt.allocPrint(allocator, "{}", .{std.fmt.fmtSliceHexLower(&buf)});
+        defer allocator.free(hex);
+
+        if (std.mem.eql(u8, hex[0..target.len], target)) {
+            // print("{s}: {d}\n", .{ hex, dec });
+            // break;
+            return dec;
+        }
+    }
+}
+
+pub fn main() !void {
+    const bingo = try mine_string("bgvyzdsv", "00000");
+    print("Part 1: {d}\n", .{bingo});
+
+    const bigger_bingo = try mine_string("bgvyzdsv", "000000");
+    print("Part 2: {d}\n", .{bigger_bingo});
+}
