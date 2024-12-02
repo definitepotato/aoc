1730c378aa49eedc38ea9b2ecc385635544892f4
definitepotato <definitepotato@gmail.com>
2024-12-01 22:02:42 -0500

    zig(day1) snek.

--- zig/2024/day1/main.zig
+++ zig/2024/day1/main.zig
@@ -7,31 +7,29 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    var left = std.ArrayList(i32).init(allocator);
-    defer left.deinit();
+    var list_left = std.ArrayList(i32).init(allocator);
+    defer list_left.deinit();
 
-    var right = std.ArrayList(i32).init(allocator);
-    defer right.deinit();
+    var list_right = std.ArrayList(i32).init(allocator);
+    defer list_right.deinit();
 
     // input here.
     var tok = std.mem.tokenize(u8, file, " \n");
     while (tok.next()) |t| {
-        const intLeft = try std.fmt.parseInt(i32, t, 10);
-        const intRight = try std.fmt.parseInt(i32, tok.next().?, 10);
+        const int_left = try std.fmt.parseInt(i32, t, 10);
+        const int_right = try std.fmt.parseInt(i32, tok.next().?, 10);
 
-        try left.append(intLeft);
-        try right.append(intRight);
+        try list_left.append(int_left);
+        try list_right.append(int_right);
     }
 
-    std.mem.sort(i32, left.items, {}, comptime std.sort.asc(i32));
-    std.mem.sort(i32, right.items, {}, comptime std.sort.asc(i32));
-    // print("[DEBUG] {any}\n", .{left.items});
-    // print("[DEBUG] {any}\n", .{right.items});
+    std.mem.sort(i32, list_left.items, {}, comptime std.sort.asc(i32));
+    std.mem.sort(i32, list_right.items, {}, comptime std.sort.asc(i32));
 
     // part1: calculate distance
     var total_distance: u32 = 0;
-    for (left.items, 0..) |_, i| {
-        total_distance += @abs(left.items[i] - right.items[i]);
+    for (list_left.items, 0..) |_, i| {
+        total_distance += @abs(list_left.items[i] - list_right.items[i]);
     }
 
     print("Part 1: {d}\n", .{total_distance});
@@ -39,13 +37,13 @@ pub fn main() !void {
     // part2: calculate similarity
     var total_similarity: i32 = 0;
     var count_similarity: i32 = 0;
-    for (left.items) |numLeft| {
-        for (right.items) |numRight| {
-            if (numLeft == numRight) {
+    for (list_left.items) |num_left| {
+        for (list_right.items) |num_right| {
+            if (num_left == num_right) {
                 count_similarity += 1;
             }
         }
-        total_similarity += numLeft * count_similarity;
+        total_similarity += num_left * count_similarity;
         count_similarity = 0;
     }
 
