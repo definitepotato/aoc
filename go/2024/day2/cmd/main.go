9bb1e5ec91f762635ad19a2f73701b1ca77d867a
definitepotato <definitepotato@gmail.com>
2024-12-02 10:09:23 -0500

    go(day2) solved.

---
+++ go/2024/day2/cmd/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"helpers"
+	"math"
+	"strings"
+)
+
+func diffIsSafe(x, y int) bool {
+	xFloat, yFloat := float64(x), float64(y)
+	difference := math.Abs(xFloat - yFloat)
+
+	if difference >= 1 && difference <= 3 {
+		return true
+	}
+
+	return false
+}
+
+func isIncreasing(list []string) bool {
+	listInt := helpers.SliceStringToInt(list)
+
+	for idx := range listInt {
+		// Toss the first value.
+		if idx == 0 {
+			continue
+		}
+
+		if listInt[idx-1] > listInt[idx] {
+			return false
+		}
+
+		if !diffIsSafe(listInt[idx-1], listInt[idx]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isDecreasing(list []string) bool {
+	listInt := helpers.SliceStringToInt(list)
+
+	for idx := range listInt {
+		// Toss the first value.
+		if idx == 0 {
+			continue
+		}
+
+		if listInt[idx-1] < listInt[idx] {
+			return false
+		}
+
+		if !diffIsSafe(listInt[idx-1], listInt[idx]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func removeIdx(slice []string, idx int) []string {
+	// go slices are pointers so we need to deep copy here
+	// to avoid modifying the original slice
+	c := make([]string, len(slice))
+	copy(c, slice)
+
+	return append(c[:idx], c[idx+1:]...)
+}
+
+func dampen(report []string) bool {
+	for idx := 0; idx < len(report); idx++ {
+		modifiedReport := removeIdx(report, idx)
+
+		if isIncreasing(modifiedReport) {
+			return true
+		}
+
+		if isDecreasing(modifiedReport) {
+			return true
+		}
+
+	}
+
+	return false
+}
+
+func main() {
+	file := helpers.ReadFile("../input.txt")
+
+	safeReports := 0
+	safeDampenedReports := 0
+	isSafe := false
+	for _, report := range file {
+		reportArray := strings.Split(report, " ")
+
+		isSafe = isIncreasing(reportArray)
+		if !isSafe {
+			isSafe = isDecreasing(reportArray)
+		}
+
+		if isSafe {
+			safeReports += 1
+		}
+
+		if !isSafe {
+			if dampen(reportArray) {
+				safeDampenedReports += 1
+			}
+		}
+	}
+
+	fmt.Printf("Part 1: %d\n", safeReports)
+	fmt.Printf("Part 2: %d\n", safeDampenedReports+safeReports)
+}
---
+++ go/2024/day2/go.mod
@@ -0,0 +1,6 @@
+module day2
+
+go 1.23.0
+
+replace helpers => ../helpers
+require helpers v1.0.0
