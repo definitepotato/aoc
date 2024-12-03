d59ed187a731ed7a19d94278b389bd6b1abe79a6
definitepotato <definitepotato@gmail.com>
2024-12-03 09:17:11 -0500

    go(day3) solved.

--- go/2024/day3/cmd/main.go
+++ go/2024/day3/cmd/main.go
@@ -7,7 +7,10 @@ import (
 	"strconv"
 )
 
-const TestInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+const (
+	TestInput1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	TestInput2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+)
 
 func getNumbersFromInstruction(input string) (int, int) {
 	rgx, _ := regexp.Compile(`mul\((?P<n1>\d+),(?P<n2>\d+)\)`)
@@ -19,25 +22,51 @@ func getNumbersFromInstruction(input string) (int, int) {
 	return leftNum, rightNum
 }
 
-func getInstructions(input string) []string {
-	rgx, _ := regexp.Compile(`mul\((?P<n1>\d+),(?P<n2>\d+)\)`)
+func getInstructions(input string, rgx *regexp.Regexp) []string {
 	instructions := rgx.FindAllString(input, -1)
-
 	return instructions
 }
 
 func main() {
 	inputs := helpers.ReadFile("../input.txt")
 
-	result := 0
+	result1 := 0
+	rgx1, _ := regexp.Compile(`mul\((?P<n1>\d+),(?P<n2>\d+)\)`)
 	for _, input := range inputs {
-		instructions := getInstructions(input)
+		instructions := getInstructions(input, rgx1)
 
 		for _, instr := range instructions {
 			n1, n2 := getNumbersFromInstruction(instr)
-			result += n1 * n2
+			result1 += n1 * n2
 		}
 	}
 
-	fmt.Printf("Part 1: %d\n", result)
+	fmt.Printf("Part 1: %d\n", result1)
+
+	result2 := 0
+	process := true
+	rgx2, _ := regexp.Compile(`(?:(?P<dont>don't)\(\)|(?<do>do)\(\))|mul\((?P<n1>\d+),(?P<n2>\d+)\)`)
+
+	for _, input := range inputs {
+		instructions := getInstructions(input, rgx2)
+
+		for _, instr := range instructions {
+			if instr == "don't()" {
+				process = false
+				continue
+			}
+
+			if instr == "do()" {
+				process = true
+				continue
+			}
+
+			if process {
+				n1, n2 := getNumbersFromInstruction(instr)
+				result2 += n1 * n2
+			}
+		}
+	}
+
+	fmt.Printf("Part 2: %d\n", result2)
 }
