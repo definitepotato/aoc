21fc1f94c16f12d5c5d99ce45d74728f50214f4a
definitepotato <definitepotato@gmail.com>
2024-12-10 20:28:01 -0500

    go(day10) part 1.

--- go/2024/day10/cmd/main.go
+++ go/2024/day10/cmd/main.go
@@ -60,20 +60,20 @@ func traverse(startPos string, grid []string, points map[string]int) {
 
 	visited := make(map[string]bool, 0)
 
-	fmt.Println("=================================")
-	fmt.Printf("Starting: %s\n", startPos)
+	// fmt.Println("=================================")
+
 	iteration := 0
 
 	for !stack.IsEmpty() {
 		iteration += 1
-		fmt.Println("---------------------------------")
-		fmt.Printf("Iteration: %d\n", iteration)
+		// fmt.Println("---------------------------------")
+		// fmt.Printf("Iteration: %d\n", iteration)
 
 		curPos := stack.Pop()
 		pointY, _ := strconv.Atoi(strings.Split(curPos, ",")[0])
 		pointX, _ := strconv.Atoi(strings.Split(curPos, ",")[1])
 		curPosN, _ := strconv.Atoi(string(grid[pointY][pointX]))
-		fmt.Printf("Popping: %s => %d\n", curPos, curPosN)
+		// fmt.Printf("Popping: %s => %d\n", curPos, curPosN)
 
 		right := pointX < len(grid[0])-1
 		left := pointX > 0
@@ -81,93 +81,89 @@ func traverse(startPos string, grid []string, points map[string]int) {
 		down := pointY < len(grid)-1
 
 		if right {
-			fmt.Printf("Looking right ... ")
+			// fmt.Printf("Looking right ... ")
 			nextPos := fmt.Sprintf("%s,%s", strconv.Itoa(pointY), strconv.Itoa(pointX+1))
 			nextPosN, _ := strconv.Atoi(string(grid[pointY][pointX+1]))
-			fmt.Printf("found %d@%s\n", nextPosN, nextPos)
+			// fmt.Printf("found %d@%s\n", nextPosN, nextPos)
 
 			if nextPosN == 9 && curPosN == 8 {
 				if visited[nextPos] {
-					fmt.Printf("Already scored: %s\n", nextPos)
-					continue
+					// fmt.Printf("Already scored: %s\n", nextPos)
+				} else {
+					points[startPos] += 1
+					visited[nextPos] = true
+					// fmt.Printf("Score: %s => %d\n", startPos, points[startPos])
 				}
-
-				points[startPos] += 1
-				visited[nextPos] = true
-				fmt.Printf("Score: %s => %d\n", startPos, points[startPos])
 			}
 
 			if nextPosN == curPosN+1 && curPosN != 8 {
-				fmt.Printf("Pushing: %s => %d\n", nextPos, nextPosN)
+				// fmt.Printf("Pushing: %s => %d\n", nextPos, nextPosN)
 				stack.Push(nextPos)
 			}
 		}
 
 		if left {
-			fmt.Printf("Looking left ... ")
+			// fmt.Printf("Looking left ... ")
 			nextPos := fmt.Sprintf("%s,%s", strconv.Itoa(pointY), strconv.Itoa(pointX-1))
 			nextPosN, _ := strconv.Atoi(string(grid[pointY][pointX-1]))
-			fmt.Printf("found %d@%s\n", nextPosN, nextPos)
+			// fmt.Printf("found %d@%s\n", nextPosN, nextPos)
 
 			if nextPosN == 9 && curPosN == 8 {
 				if visited[nextPos] {
-					fmt.Printf("Already scored: %s\n", nextPos)
-					continue
+					// fmt.Printf("Already scored: %s\n", nextPos)
+				} else {
+					points[startPos] += 1
+					visited[nextPos] = true
+					// fmt.Printf("Score: %s => %d\n", startPos, points[startPos])
 				}
-
-				points[startPos] += 1
-				visited[nextPos] = true
-				fmt.Printf("Score: %s => %d\n", startPos, points[startPos])
 			}
 
 			if nextPosN == curPosN+1 && curPosN != 8 {
-				fmt.Printf("Pushing: %s => %d\n", nextPos, nextPosN)
+				// fmt.Printf("Pushing: %s => %d\n", nextPos, nextPosN)
 				stack.Push(nextPos)
 			}
 		}
 
 		if up {
-			fmt.Printf("Looking up ... ")
+			// fmt.Printf("Looking up ... ")
 			nextPos := fmt.Sprintf("%s,%s", strconv.Itoa(pointY-1), strconv.Itoa(pointX))
 			nextPosN, _ := strconv.Atoi(string(grid[pointY-1][pointX]))
-			fmt.Printf("found %d@%s\n", nextPosN, nextPos)
+			// fmt.Printf("found %d@%s\n", nextPosN, nextPos)
 
 			if nextPosN == 9 && curPosN == 8 {
 				if visited[nextPos] {
-					fmt.Printf("Already scored: %s\n", nextPos)
-					continue
+					// fmt.Printf("Already scored: %s\n", nextPos)
+				} else {
+					points[startPos] += 1
+					visited[nextPos] = true
+					// fmt.Printf("Score: %s => %d\n", startPos, points[startPos])
 				}
-
-				points[startPos] += 1
-				visited[nextPos] = true
-				fmt.Printf("Score: %s => %d\n", startPos, points[startPos])
 			}
 
 			if nextPosN == curPosN+1 && curPosN != 8 {
-				fmt.Printf("Pushing: %s => %d\n", nextPos, nextPosN)
+				// fmt.Printf("Pushing: %s => %d\n", nextPos, nextPosN)
 				stack.Push(nextPos)
 			}
 		}
 
 		if down {
-			fmt.Printf("Looking down ... ")
+			// fmt.Printf("Looking down ... ")
 			nextPos := fmt.Sprintf("%s,%s", strconv.Itoa(pointY+1), strconv.Itoa(pointX))
 			nextPosN, _ := strconv.Atoi(string(grid[pointY+1][pointX]))
-			fmt.Printf("found %d@%s\n", nextPosN, nextPos)
+			// fmt.Printf("found %d@%s\n", nextPosN, nextPos)
 
 			if nextPosN == 9 && curPosN == 8 {
 				if visited[nextPos] {
-					fmt.Printf("Already scored: %s\n", nextPos)
-					continue
+					// fmt.Printf("Already scored: %s\n", nextPos)
+				} else {
+					points[startPos] += 1
+					visited[nextPos] = true
+					// fmt.Printf("Score: %s => %d\n", startPos, points[startPos])
 				}
-
-				points[startPos] += 1
-				visited[nextPos] = true
-				fmt.Printf("Score: %s => %d\n", startPos, points[startPos])
 			}
 
 			if nextPosN == curPosN+1 && curPosN != 8 {
-				fmt.Printf("Pushing: %s => %d\n", nextPos, nextPosN)
+				// fmt.Printf("Pushing: %s => %d\n", nextPos, nextPosN)
 				stack.Push(nextPos)
 			}
 		}
