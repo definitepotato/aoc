db98bc687254e0e692222a50d275cd2c9194b0f8
definitepotato <definitepotato@gmail.com>
2025-03-18 15:27:48 -0400

    rust(day01) part 2.

--- rust/2015/day01/src/main.rs
+++ rust/2015/day01/src/main.rs
@@ -14,23 +14,38 @@ fn main() {
     // let input = r#")())())"#;
     let lines = lines_from_file(String::from("src/input.txt"));
 
+    let mut index: u32 = 0;
     let mut floor: i32 = 0;
+    let mut basement_floor: u32 = 0;
 
     for line in lines {
         for c in line.chars() {
+            if basement_floor == 0 {
+                index += 1;
+            }
             match c {
                 ')' => {
                     floor -= 1;
+                    if floor < 0 {
+                        basement_floor = index;
+                    }
                 }
                 '(' => {
                     floor += 1;
+                    if floor < 0 {
+                        basement_floor = index;
+                    }
                 }
                 _ => {
                     floor += 0;
+                    if floor < 0 {
+                        basement_floor = index;
+                    }
                 }
             }
         }
     }
 
     println!("Part 1: {}", floor);
+    println!("Part 2: {}", basement_floor);
 }
