9ac1a28bb600b4484677ea0cbdf7a586d24d5d05
definitepotato <definitepotato@gmail.com>
2025-07-05 09:57:59 -0400

    zig(day01)[2016] part 2

--- zig/2016/day01/main.zig
+++ zig/2016/day01/main.zig
@@ -14,6 +14,39 @@ const Pos = struct {
     x: i32,
     y: i32,
     dir: direction,
+    allocator: std.mem.Allocator,
+    visited: std.ArrayList(vec),
+    overlap: std.ArrayList(vec),
+
+    pub fn init(allocator: std.mem.Allocator) Pos {
+        return .{
+            .x = 0,
+            .y = 0,
+            .dir = .North,
+            .allocator = allocator,
+            .visited = std.ArrayList(vec).init(allocator),
+            .overlap = std.ArrayList(vec).init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *Pos) void {
+        self.visited.deinit();
+        self.overlap.deinit();
+    }
+
+    pub fn seen(self: *Pos, pos: vec) bool {
+        if (self.visited.items.len == 0) {
+            return false;
+        }
+
+        for (self.visited.items) |item| {
+            if (std.meta.eql(item, pos)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     pub fn turn(self: *Pos, turn_dir: u8) void {
         if (turn_dir == 'R') {
@@ -35,21 +68,49 @@ const Pos = struct {
         }
     }
 
-    pub fn move(self: *Pos, steps: i32) void {
+    pub fn move(self: *Pos, steps: usize) !void {
         if (self.dir == .North) {
-            self.y += steps;
+            for (0..steps) |_| {
+                self.y += 1;
+                const pos = vec{ .x = self.x, .y = self.y };
+                if (self.seen(pos)) {
+                    try self.overlap.append(pos);
+                }
+                try self.visited.append(pos);
+            }
         }
 
         if (self.dir == .East) {
-            self.x += steps;
+            for (0..steps) |_| {
+                self.x += 1;
+                const pos = vec{ .x = self.x, .y = self.y };
+                if (self.seen(pos)) {
+                    try self.overlap.append(pos);
+                }
+                try self.visited.append(pos);
+            }
         }
 
         if (self.dir == .South) {
-            self.y -= steps;
+            for (0..steps) |_| {
+                self.y -= 1;
+                const pos = vec{ .x = self.x, .y = self.y };
+                if (self.seen(pos)) {
+                    try self.overlap.append(pos);
+                }
+                try self.visited.append(pos);
+            }
         }
 
         if (self.dir == .West) {
-            self.x -= steps;
+            for (0..steps) |_| {
+                self.x -= 1;
+                const pos = vec{ .x = self.x, .y = self.y };
+                if (self.seen(pos)) {
+                    try self.overlap.append(pos);
+                }
+                try self.visited.append(pos);
+            }
         }
     }
 
@@ -59,54 +120,60 @@ const Pos = struct {
 };
 
 test "turn position" {
-    var my_pos = Pos{
-        .x = 0,
-        .y = 0,
-        .dir = .North,
-    };
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var my_pos = Pos.init(allocator);
+    defer my_pos.deinit();
 
     my_pos.turn('R');
     try std.testing.expect(my_pos.dir == .East);
 }
 
 test "move position" {
-    var my_pos = Pos{
-        .x = 0,
-        .y = 0,
-        .dir = .North,
-    };
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
 
-    my_pos.move(2.0);
+    var my_pos = Pos.init(allocator);
+    defer my_pos.deinit();
+
+    try my_pos.move(2.0);
     try std.testing.expect(my_pos.y == 2);
 
     my_pos.turn('L');
-    my_pos.move(2.0);
+    try my_pos.move(2.0);
     try std.testing.expect(my_pos.x == -2);
 }
 
 const vec = struct {
     x: i32,
     y: i32,
+
+    pub fn distance(self: *vec) u32 {
+        return @abs(self.x) + @abs(self.y);
+    }
 };
 
-const test_input = "R5, L5, R5, R3";
+const test_input = "R8, R4, R4, R8";
 
 pub fn main() !void {
-    var my_pos = Pos{
-        .x = 0,
-        .y = 0,
-        .dir = .North,
-    };
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var my_pos = Pos.init(allocator);
+    defer my_pos.deinit();
 
     var lines = std.mem.tokenizeAny(u8, input, ", \n");
     while (lines.next()) |line| {
-        // const int_move: u8 = line[1] - '0'; // convert u8 to integer
-        const int_move = try std.fmt.parseInt(i32, line[1..], 10);
+        const int_move = try std.fmt.parseInt(usize, line[1..], 10);
 
         my_pos.turn(line[0]);
-        my_pos.move(int_move);
-        // print("{s}: {}\n", .{ line, my_pos });
+        try my_pos.move(int_move);
     }
 
     print("{d}\n", .{my_pos.distance()});
+    print("{d}\n", .{my_pos.overlap.items[0].distance()});
 }
