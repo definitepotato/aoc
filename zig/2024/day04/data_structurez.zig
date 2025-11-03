c3933f06c077478e58dbbd0c7ab3c99613d59615
definitepotato <definitepotato@gmail.com>
2025-11-03 10:11:36 -0500

    task(day04)[2024] update lib.

--- zig/2024/day04/data_structurez.zig
+++ zig/2024/day04/data_structurez.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+/// Stores sequence of Nodes which each hold a value of type `T`.
 pub fn LinkedList(comptime T: type) type {
     return struct {
         const Self = @This();
@@ -11,23 +12,29 @@ pub fn LinkedList(comptime T: type) type {
 
         head: ?*Node = null,
         allocator: std.mem.Allocator,
+        len: usize,
 
+        /// Creates a managed LinkedList with a `null` head.
         pub fn init(allocator: std.mem.Allocator) Self {
-            return .{ .head = null, .allocator = allocator };
+            return .{ .head = null, .allocator = allocator, .len = 0 };
         }
 
+        /// Add a new Node to the beginning of the LinkedList with a `value`.
         pub fn prepend(self: *Self, value: T) !void {
             const new_node = try self.allocator.create(Node);
             new_node.* = Node{ .value = value, .next = self.head };
             self.head = new_node;
+            self.len += 1;
         }
 
+        /// Add a new Node to the end of the LinkedList with a `value`.
         pub fn append(self: *Self, value: T) !void {
             const new_node = try self.allocator.create(Node);
             new_node.* = Node{ .value = value, .next = null };
 
             if (self.head == null) {
                 self.head = new_node;
+                self.len += 1;
                 return;
             }
 
@@ -36,8 +43,10 @@ pub fn LinkedList(comptime T: type) type {
                 current = next_node;
             }
             current.?.next = new_node;
+            self.len += 1;
         }
 
+        /// Insert a new Node with `value` after an existing Node with `after_value`.
         pub fn insert(self: *Self, after_value: T, value: T) !void {
             var current = self.head;
             while (current) |node| {
@@ -45,12 +54,15 @@ pub fn LinkedList(comptime T: type) type {
                     const new_node = try self.allocator.create(Node);
                     new_node.* = Node{ .value = value, .next = node.next };
                     node.next = new_node;
+                    self.len += 1;
                     return;
                 }
                 current = node.next;
+                self.len += 1;
             }
         }
 
+        /// Print the LinkedList to the terminal.
         pub fn print(self: *Self) void {
             var current = self.head;
             while (current) |node| {
@@ -60,6 +72,7 @@ pub fn LinkedList(comptime T: type) type {
             std.debug.print("null\n", .{});
         }
 
+        /// Release the backing buffer.
         pub fn deinit(self: *Self) void {
             var current = self.head;
             while (current) |node| {
@@ -72,6 +85,7 @@ pub fn LinkedList(comptime T: type) type {
     };
 }
 
+/// Stores a two-deminsional grid as rows and columns.
 pub fn Matrix(comptime T: type) type {
     return struct {
         allocator: std.mem.Allocator,
@@ -81,6 +95,7 @@ pub fn Matrix(comptime T: type) type {
 
         const Self = @This();
 
+        /// Creates a managed Matrix of size `width` x `height`.
         pub fn init(allocator: std.mem.Allocator, width: usize, height: usize) !Self {
             const buffer = try allocator.alloc(T, width * height);
 
@@ -92,16 +107,21 @@ pub fn Matrix(comptime T: type) type {
             };
         }
 
+        /// Create a managed Matrix from an input file at `path`. Path should include filename.
         pub fn initFromFile(allocator: std.mem.Allocator, path: []const u8) !Self {
-            // Read file into memory.
+            // Get a file descriptor.
             const file = try std.fs.cwd().openFile(path, .{});
             defer file.close();
 
+            // Load file contents into memory.
             const contents = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
             defer allocator.free(contents);
 
+            // Trim leading/trailing newlines or whitespace.
+            const trimmed = std.mem.trim(u8, contents, "\r\n ");
+
             // Split by lines.
-            var lines = std.mem.splitScalar(u8, std.mem.trim(u8, contents, "\r\n"), '\n');
+            var lines = std.mem.splitScalar(u8, trimmed, '\n');
 
             // First pass: determine width and height.
             var width: usize = 0;
@@ -125,6 +145,7 @@ pub fn Matrix(comptime T: type) type {
             return matrix;
         }
 
+        /// Create a managed Matrix from a multi-line const/var.
         pub fn initFromText(allocator: std.mem.Allocator, text: []const u8) !Self {
             // Trim leading/trailing newlines or whitespace.
             const trimmed = std.mem.trim(u8, text, "\r\n ");
@@ -154,18 +175,22 @@ pub fn Matrix(comptime T: type) type {
             return matrix;
         }
 
+        /// Release the backing buffer.
         pub fn deinit(self: *Self) void {
             self.allocator.free(self.buffer);
         }
 
+        /// Get the value at coord `x`,`y`.
         pub fn get(self: *const Self, x: usize, y: usize) T {
             return self.buffer[y * self.width + x];
         }
 
+        /// Set `value` at coord `x`,`y`.
         pub fn set(self: *Self, x: usize, y: usize, value: T) void {
             self.buffer[y * self.width + x] = value;
         }
 
+        /// Print the Matrix to the terminal.
         pub fn print(self: *Self) void {
             for (0..self.height) |y| {
                 for (0..self.width) |x| {
@@ -177,6 +202,7 @@ pub fn Matrix(comptime T: type) type {
     };
 }
 
+/// A First-In, First-Out abstraction.
 pub fn Queue(comptime T: type) type {
     return struct {
         const Self = @This();
@@ -187,6 +213,7 @@ pub fn Queue(comptime T: type) type {
         back: usize,
         count: usize,
 
+        /// Create an empty Queue.
         pub fn init(allocator: std.mem.Allocator) Self {
             return Self{
                 .allocator = allocator,
@@ -197,6 +224,7 @@ pub fn Queue(comptime T: type) type {
             };
         }
 
+        /// Release the backing buffer.
         pub fn deinit(self: *Self) void {
             self.allocator.free(self.buffer);
         }
@@ -215,6 +243,7 @@ pub fn Queue(comptime T: type) type {
             self.back = self.count;
         }
 
+        /// Add a new `item` to the back of the Queue.
         pub fn enqueue(self: *Self, item: T) !void {
             if (self.count == self.buffer.len) {
                 try self.resize(if (self.buffer.len == 0) 1 else self.buffer.len * 2);
@@ -224,6 +253,7 @@ pub fn Queue(comptime T: type) type {
             self.count += 1;
         }
 
+        /// Fetch an `item` from the front of the Queue.
         pub fn dequeue(self: *Self) ?T {
             if (self.count == 0) return null;
             const item = self.buffer[self.front];
@@ -232,17 +262,23 @@ pub fn Queue(comptime T: type) type {
             return item;
         }
 
+        /// Peek at the next item in the Queue without fetching it.
         pub fn peek(self: *Self) ?T {
             if (self.count == 0) return null;
             return self.buffer[self.front];
         }
 
+        /// Returns `true` if the Queue is empty, otherwise `false`.
         pub fn is_empty(self: *Self) bool {
             return self.count == 0;
         }
     };
 }
 
+/// A variable length structure to an underlying array of type `T`. Automatically
+/// resizes as needed. Unsafe to use buffer directly as the `resize()` will include
+/// junk data in the backing buffer. Tracks the end of the valid data in the backing
+/// buffer using `len`.
 pub fn Slice(comptime T: type) type {
     return struct {
         const Self = @This();
@@ -251,6 +287,7 @@ pub fn Slice(comptime T: type) type {
         buffer: []T,
         len: usize,
 
+        /// Create an empty Slice.
         pub fn init(allocator: std.mem.Allocator) Self {
             return Self{
                 .allocator = allocator,
@@ -259,6 +296,7 @@ pub fn Slice(comptime T: type) type {
             };
         }
 
+        /// Release the backing buffer.
         pub fn deinit(self: *Self) void {
             self.allocator.free(self.buffer);
         }
@@ -270,7 +308,9 @@ pub fn Slice(comptime T: type) type {
             self.buffer = new_buffer;
         }
 
+        /// Add a new `item` to the end of the buffer.
         pub fn append(self: *Self, item: T) !void {
+            // Golang style resizing, we double capacity of buffer if we exceed the boundary.
             if (self.len >= self.buffer.len) {
                 try self.resize(if (self.buffer.len == 0) 1 else self.buffer.len * 2);
             }
@@ -279,6 +319,30 @@ pub fn Slice(comptime T: type) type {
             self.len += 1;
         }
 
+        /// Returns a sliding window iterator with a width of `size`.
+        pub fn window(self: *Self, size: usize) SliceWindowIterator {
+            return SliceWindowIterator{ .slice = self, .window_size = size, .window_start = 0 };
+        }
+
+        pub const SliceWindowIterator = struct {
+            slice: *Self,
+            window_size: usize,
+            window_start: usize,
+
+            /// Iterates the backing buffer in a sliding window.
+            pub fn next(self: *SliceWindowIterator) ?[]T {
+                const window_end = self.window_start + self.window_size;
+                if (window_end > self.slice.len) {
+                    return null;
+                }
+
+                const window_slice = self.slice.buffer[self.window_start..window_end];
+                self.window_start += 1;
+                return window_slice;
+            }
+        };
+
+        /// Returns an iterator, use `next()` to iterate.
         pub fn iterator(self: *Self) SliceIterator {
             return SliceIterator{ .slice = self, .index = 0 };
         }
@@ -287,6 +351,7 @@ pub fn Slice(comptime T: type) type {
             slice: *Self,
             index: usize,
 
+            /// Iterates the backing buffer item by item.
             pub fn next(self: *SliceIterator) ?T {
                 if (self.index >= self.slice.len) {
                     return null;
@@ -297,9 +362,25 @@ pub fn Slice(comptime T: type) type {
                 return item;
             }
         };
+
+        /// The caller owns the returned memory. Does not empty the backing buffer.
+        pub fn toOwned(self: Self) !?[]T {
+            if (self.len > 0) {
+                const items = self.buffer[0..self.len];
+                if (self.allocator.alloc(T, items.len)) |slice_Ts| {
+                    std.mem.copyForwards(T, slice_Ts, items);
+                    return slice_Ts;
+                } else |_| {
+                    return std.mem.Allocator.Error.OutOfMemory;
+                }
+            }
+
+            return null;
+        }
     };
 }
 
+/// A Last-In, First-Out abstraction.
 pub fn Stack(comptime T: type) type {
     return struct {
         const Self = @This();
@@ -308,10 +389,7 @@ pub fn Stack(comptime T: type) type {
         buffer: []T,
         top: usize,
 
-        pub const Error = error{
-            OutOfMemory,
-        };
-
+        /// Create an empty Stack.
         pub fn init(allocator: std.mem.Allocator) Self {
             return Self{
                 .allocator = allocator,
@@ -320,6 +398,7 @@ pub fn Stack(comptime T: type) type {
             };
         }
 
+        /// Release the backing buffer.
         pub fn deinit(self: *Self) void {
             self.allocator.free(self.buffer);
         }
@@ -331,6 +410,7 @@ pub fn Stack(comptime T: type) type {
             self.buffer = new_buffer;
         }
 
+        /// Push an `item` onto the stop of the Stack.
         pub fn push(self: *Self, item: T) !void {
             if (self.top >= self.buffer.len) {
                 try self.resize(if (self.buffer.len == 0) 1 else self.buffer.len * 2);
@@ -339,29 +419,33 @@ pub fn Stack(comptime T: type) type {
             self.top += 1;
         }
 
+        /// Fetch an item from the top of the Stack.
         pub fn pop(self: *Self) ?T {
             if (self.top == 0) return null;
             self.top -= 1;
             return self.buffer[self.top];
         }
 
+        /// Peek at the next item in the Stack without fetching it.
         pub fn peek(self: *Self) ?T {
             if (self.top == 0) return null;
             return self.buffer[self.top - 1];
         }
 
+        /// Returns `true` if the Stack is empty, otherwise `false`.
         pub fn is_empty(self: *Self) bool {
             return self.top == 0;
         }
 
-        pub fn toOwned(self: Self) Error!?[]T {
+        /// The caller owns the returned memory. Does not empty the backing buffer.
+        pub fn toOwned(self: Self) !?[]T {
             if (self.top > 0) {
                 const items = self.buffer[0..self.top];
                 if (self.allocator.alloc(T, items.len)) |slice_Ts| {
                     std.mem.copyForwards(T, slice_Ts, items);
                     return slice_Ts;
                 } else |_| {
-                    return Error.OutOfMemory;
+                    return std.mem.Allocator.Error.OutOfMemory;
                 }
             }
 
