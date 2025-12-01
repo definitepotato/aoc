36130ff8e96dd7192ae2f3a3e2e2482de12c26e0
definitepotato <definitepotato@gmail.com>
2025-12-01 16:28:51 -0500

    go(day01)[2025] part 2 wip.

--- go/2025/day01/main.go
+++ go/2025/day01/main.go
@@ -34,24 +34,36 @@ func main() {
 	start := dial
 
 	part1 := 0
+	part2 := 0
 
 	for _, line := range lines {
 		dir := line[0]
 		distance, _ := strconv.Atoi(line[1:])
 
+		turns := distance / 100
+		part2 += turns
+
 		if dir == 'R' {
+			if dial+distance%100 >= 100 {
+				part2 += 1
+			}
 			dial += distance
 		}
 
 		if dir == 'L' {
+			if dial > 0 && (dial-distance%100) <= 0 {
+				part2 += 1
+			}
 			start = (100 - start) % 100
 			dial -= distance
 		}
 
-		if dial%100 == 0 {
+		dial = dial % 100
+		if dial == 0 {
 			part1 += 1
 		}
 	}
 
 	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", part2)
 }
