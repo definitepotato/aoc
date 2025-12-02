edb8362a3c3e732bea3502a766be9d8a48088910
definitepotato <definitepotato@gmail.com>
2025-12-02 10:43:08 -0500

    zig(day02)[2025] added memo.

--- zig/2025/day02/main.zig
+++ zig/2025/day02/main.zig
@@ -4,9 +4,12 @@ const assert = std.debug.assert;
 const input_file = @embedFile("input.txt");
 const allocator = std.heap.page_allocator;
 
-fn isValid(id: u64) bool {
-    const id_str = std.fmt.allocPrint(allocator, "{d}", .{id}) catch unreachable;
+fn isValid(id: u64, memo: *std.AutoHashMap(u64, bool)) bool {
+    if (memo.get(id)) |is_valid| {
+        return is_valid;
+    }
 
+    const id_str = std.fmt.allocPrint(allocator, "{d}", .{id}) catch unreachable;
     if (id_str.len % 2 == 1) return true;
 
     const half_id_len = id_str.len / 2;
@@ -14,9 +17,11 @@ fn isValid(id: u64) bool {
     const right = id_str[half_id_len..];
 
     for (0..half_id_len) |idx| {
+        memo.*.put(id, true) catch unreachable;
         if (left[idx] != right[idx]) return true;
     }
 
+    memo.*.put(id, false) catch unreachable;
     return false;
 }
 
@@ -24,18 +29,16 @@ const test_input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,16
 
 pub fn main() void {
     var acc: u64 = 0;
+    var memo: std.AutoHashMap(u64, bool) = .init(allocator);
 
     var ids = std.mem.tokenizeAny(u8, input_file, ",\n");
     while (ids.next()) |id| {
-        var start_end = std.mem.splitScalar(u8, id, '-');
-        const start = start_end.next().?;
-        const end = start_end.next().?;
+        var range = std.mem.splitScalar(u8, id, '-');
+        const start = std.fmt.parseInt(u64, range.next().?, 10) catch unreachable;
+        const end = std.fmt.parseInt(u64, range.next().?, 10) catch unreachable;
 
-        const start_int = std.fmt.parseInt(u64, start[0..], 10) catch unreachable;
-        const end_int = std.fmt.parseInt(u64, end[0..], 10) catch unreachable;
-
-        for (start_int..end_int + 1) |i| {
-            if (!isValid(i)) {
+        for (start..end + 1) |i| {
+            if (!isValid(i, &memo)) {
                 acc += i;
             }
         }
