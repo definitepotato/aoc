fa9c101af9ceaaae91e1efccc0920e0b86c35762
definitepotato <definitepotato@gmail.com>
2025-12-07 13:05:27 -0500

    zig(day07)[2025] part 2.

--- zig/2025/day07/main.zig
+++ zig/2025/day07/main.zig
@@ -25,7 +25,30 @@ const test_input =
     \\...............
 ;
 
-pub fn main() void {
+// count number of timelines produced by a tachyon
+pub fn solvePart2(memo: *std.AutoHashMap([2]usize, usize), pos: [2]usize, grid: *ds.Matrix(u8)) usize {
+    // stop when we reach the bottom
+    if (pos[1] == grid.height - 1) return 1;
+
+    // efficiency!
+    const val_ptr = memo.get(pos);
+    if (val_ptr) |val| return val;
+
+    var ans: usize = 0; // number of timelines
+
+    // particle continues straight down if '.'
+    if (grid.getXY(pos[0], pos[1]) == '.') {
+        ans = solvePart2(memo, [2]usize{ pos[0], pos[1] + 1 }, grid); // recurse next position.
+    } else { // split!!
+        if (pos[0] > 0) ans = solvePart2(memo, [2]usize{ pos[0] - 1, pos[1] + 1 }, grid); // recurse left split.
+        if (pos[0] < grid.width - 1) ans += solvePart2(memo, [2]usize{ pos[0] + 1, pos[1] + 1 }, grid); // recurse right split.
+    }
+
+    memo.put(pos, ans) catch unreachable;
+    return ans;
+}
+
+pub fn solvePart1() void {
     var part1: usize = 0;
 
     var grid = ds.Matrix(u8).initFromText(allocator, input_file) catch unreachable;
@@ -44,5 +67,26 @@ pub fn main() void {
     }
 
     print("Part 1: {d}\n", .{part1});
+}
+
+pub fn main() void {
+    solvePart1();
+
+    // find starting X
+    var start: usize = 0;
+    var input_it = std.mem.tokenizeAny(u8, input_file, "\n");
+    while (input_it.next()) |line| {
+        for (line, 0..) |c, idx| {
+            if (c == 'S') {
+                start = idx;
+            }
+        }
+    }
+
+    var map = std.AutoHashMap([2]usize, usize).init(allocator);
+    var grid = ds.Matrix(u8).initFromText(allocator, input_file) catch unreachable;
+    const part2 = solvePart2(&map, [2]usize{ start, 1 }, &grid);
+
+    print("Part 2: {d}\n", .{part2});
     arena.deinit();
 }
